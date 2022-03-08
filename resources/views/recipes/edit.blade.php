7d8628da3d6f6232f8fe4dea16f2771b76d48a09
Karin Flarup <karin.flarup@medieinstitutet.se>
2022-03-08 10:15:30 +0100

    added authentication for edit

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -8,42 +8,44 @@
 
 	<div class="card">
 		<div class="card-body">
-			<form method="POST" action="{{route('recipes.update', ['recipe' => $recipe])}}">
-				@csrf
-                @method('PUT')
+            @if($recipe->user == auth()->user())
+			    <form method="POST" action="{{route('recipes.update', ['recipe' => $recipe])}}">
+				    @csrf
+                    @method('PUT')
 
-				<div class="mb-3">
-					<label for="title" class="form-label">Title</label>
-					<input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?: $recipe->title}}" required>
+				    <div class="mb-3">
+					    <label for="title" class="form-label">Title</label>
+					    <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?: $recipe->title}}" required>
                     
-                    <label for="summary" class="form-label">Summary</label>
-					<input type="textarea" class="form-control" id="summary" name="summary" value="{{ old('summary') ?: $recipe->summary}}" required>
+                        <label for="summary" class="form-label">Summary</label>
+					    <input type="textarea" class="form-control" id="summary" name="summary" value="{{ old('summary') ?: $recipe->summary}}" required>
 
-                    <label for="description" class="form-label">Description</label>
-					<input type="textarea" class="form-control" id="description" name="description" value="{{ old('description') ?: $recipe->description}}" required>
+                        <label for="description" class="form-label">Description</label>
+					    <input type="textarea" class="form-control" id="description" name="description" value="{{ old('description') ?: $recipe->description}}" required>
 
-                    <label for="cooking_time" class="form-label">Cooking Time</label>
-					<input type="text" class="form-control" id="cooking_time" name="cooking_time" value="{{ old('cooking_time') ?: $recipe->cooking_time}}" required>
+                        <label for="cooking_time" class="form-label">Cooking Time</label>
+					    <input type="text" class="form-control" id="cooking_time" name="cooking_time" value="{{ old('cooking_time') ?: $recipe->cooking_time}}" required>
                     
-                    <label for="ingredients" class="form-label">Ingredients</label>
-					<input type="textarea" class="form-control" id="ingredients" name="ingredients" value="{{ old('ingredients') ?: $recipe->ingredients}}" required>
+                        <label for="ingredients" class="form-label">Ingredients</label>
+					    <input type="textarea" class="form-control" id="ingredients" name="ingredients" value="{{ old('ingredients') ?: $recipe->ingredients}}" required>
 
-                    <h3>Tag your post!</h3>
+                        <h3>Tag your post!</h3>
 					
-                    <select class="form-control" id="categories" name="categories[]" multiple="multiple">
-                        @foreach($categories as $category)
-                        <option value="{{ $category->id }}"
-                             @foreach($recipe->categories as $resCategory)
+                        <select class="form-control" id="categories" name="categories[]" multiple="multiple">
+                            @foreach($categories as $category)
+                            <option value="{{ $category->id }}"
+                                @foreach($recipe->categories as $resCategory)
                                    {{ $resCategory->id==$category->id?'Selected':'' }}
-                             @endforeach
-                        >{{ $category->title}}</option>
-                        @endforeach
-                     </select>
+                                @endforeach
+                            >{{ $category->title}}</option>
+                            @endforeach
+                        </select>
                     
-				</div>
+				    </div>
 
-				<button type="submit" class="btn btn-success">Uppdatera</button>
-			</form>
+				    <button type="submit" class="btn btn-success">Uppdatera</button>
+			    </form>
+            @endif
 		</div>
 	</div>
 
